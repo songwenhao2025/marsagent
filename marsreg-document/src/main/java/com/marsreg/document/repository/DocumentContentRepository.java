40dc61215f14667717fd24fcbb1c7850f8ed3868
wenhao.swh <wenhao.swh@alibaba-inc.com>
2025-05-22 19:13:20 +0800

    添加：DocumentContentRepository接口

--- marsreg-document/src/main/java/com/marsreg/document/repository/DocumentContentRepository.java
+++ marsreg-document/src/main/java/com/marsreg/document/repository/DocumentContentRepository.java
@@ -1,1 +1,12 @@
- 
+package com.marsreg.document.repository;
+
+import com.marsreg.document.entity.DocumentContent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DocumentContentRepository extends JpaRepository<DocumentContent, Long> {
+    Optional<DocumentContent> findByDocumentId(Long documentId);
+} 
